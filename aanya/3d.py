5a0800b542ff039b52116c6005e98374c2749643
Aanya1007 <cupertino+amalthankar@thecoderschool.com>
2025-06-28 15:00:43 -0700

    Update 3d.py

--- aanya/3d.py
+++ aanya/3d.py
@@ -1,4 +1,5 @@
 from ursina import *
+import time
 import random
 app = Ursina()
 
@@ -7,6 +8,10 @@ track = Entity(model="track.glb",  position = (0,-0.7,30), scale = (1,1,3))
 track.rotation_x -= 8
 lanes = [-7.5, 0, 7.5]
 obstacle = Entity(model = "ATrain.glb", position = (random.choice(lanes), 0,0,50), collider = "box")
+timer = Text(text = "Time:0", position = (0.5,0.5), background = True, scale = 3, origin = (0,1))
+dead = Text(text = "You Died!", position = (0,0), scale = 3,origin = (0,2), background = True )
+dead.enabled = False
+
 obstacle.rotation_y += 90
 obstacle.rotation_z+= 10
 def input(key):
@@ -20,6 +25,7 @@ def input(key):
             cube.x = lanes[1]
         elif cube.x == 0:
             cube.x = lanes[2]
+starttime = time.time()
 
 def update():
     obstacle.y -= 0.05
@@ -29,8 +35,14 @@ def update():
         obstacle.z = 30
         obstacle.x = random.choice(lanes)
     if obstacle.intersects(cube).hit:
-        print("dead")
-        exit()
-    
-
+        print("You Died!")
+        dead.enabled = True
+        cube.enabled = False
+        obstacle.enabled = False
+        
+        
+        
+    endtime = time.time() - starttime
+    timer.text = f"Time = %.2d"% endtime
 app.run()
+
